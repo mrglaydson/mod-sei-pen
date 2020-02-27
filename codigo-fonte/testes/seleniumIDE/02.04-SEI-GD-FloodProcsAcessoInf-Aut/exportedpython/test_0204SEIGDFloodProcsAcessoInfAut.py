db8a53ed98900d68aaf6e1518ed20ead192db305
Marcelo Linhares Castro <marlinhares@gmail.com>
2020-02-27 10:50:45 -0300

    Ajustes testes

--- codigo-fonte/testes/seleniumIDE/02.04-SEI-GD-FloodProcsAcessoInf-Aut/exportedpython/test_0204SEIGDFloodProcsAcessoInfAut.py
+++ codigo-fonte/testes/seleniumIDE/02.04-SEI-GD-FloodProcsAcessoInf-Aut/exportedpython/test_0204SEIGDFloodProcsAcessoInfAut.py
@@ -70,7 +70,7 @@ class Test0204SEIGDFloodProcsAcessoInfAut():
       dropdown.find_element(By.XPATH, "//option[. = 'Cópia Autenticada Administrativamente']").click()
       self.driver.find_element(By.ID, "selTipoConferencia").click()
       self.driver.find_element(By.ID, "optPublico").click()
-      self.driver.find_element(By.ID, "filArquivo").send_keys("/home/marcelo/projetos/git/planejamento/sei-mod-gestao-documental/codigo-fonte/testes/seleniumIDE/02.04-SEI-GD-FloodProcsAcessoInf-Aut/teste.pdf")
+      self.driver.find_element(By.ID, "filArquivo").send_keys("teste.pdf")
       WebDriverWait(self.driver, 30000).until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, ".infraTd:nth-child(2)")))
       self.driver.find_element(By.CSS_SELECTOR, ".infraTd:nth-child(2)").click()
       assert self.driver.find_element(By.CSS_SELECTOR, ".infraTd:nth-child(2) > div").text == "teste.pdf"
