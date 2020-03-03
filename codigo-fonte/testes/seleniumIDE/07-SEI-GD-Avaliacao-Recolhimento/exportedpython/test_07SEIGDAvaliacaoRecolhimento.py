99bdfd198956f87e5df0150e46ad426f7f802bf8
Marcelo Linhares Castro <marlinhares@gmail.com>
2020-03-03 14:51:29 -0300

    Ajustes testes

--- codigo-fonte/testes/seleniumIDE/07-SEI-GD-Avaliacao-Recolhimento/exportedpython/test_07SEIGDAvaliacaoRecolhimento.py
+++ codigo-fonte/testes/seleniumIDE/07-SEI-GD-Avaliacao-Recolhimento/exportedpython/test_07SEIGDAvaliacaoRecolhimento.py
@@ -41,6 +41,7 @@ class Test07SEIGDAvaliacaoRecolhimento():
     self.driver.switch_to.alert.accept()
     self.driver.find_element(By.XPATH, "//div[@id=\'divInfraAreaTabela\']/table/tbody/tr[2]/td[11]/a/img").click()
     self.driver.switch_to.alert.accept()
+    WebDriverWait(self.driver, 30000).until(expected_conditions.visibility_of_element_located((By.XPATH, "//*[@id=\"divInfraBarraLocalizacao\"]")))
   
   def test_0750MandaListagemRecolhimento(self):
     self.driver.get("http://seigd.intra.planejamento//sip/login.php?sigla_orgao_sistema=ME&sigla_sistema=SEI&infra_url=L3NlaS8=")
