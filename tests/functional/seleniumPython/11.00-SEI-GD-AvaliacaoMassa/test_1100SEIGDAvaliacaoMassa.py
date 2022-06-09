853213a6f10070a02172714619674d3c124dc1c8
higocavalcante <higodf@gmail.com>
2022-06-08 22:24:17 -0300

    Inclusão do campo Especificação nas funcionalidades de preparar listagem de eliminação e recolhimento
    closes #150

--- tests/functional/seleniumPython/11.00-SEI-GD-AvaliacaoMassa/test_1100SEIGDAvaliacaoMassa.py
+++ tests/functional/seleniumPython/11.00-SEI-GD-AvaliacaoMassa/test_1100SEIGDAvaliacaoMassa.py
@@ -144,7 +144,7 @@ class Test1100SEIGDAvaliacaoMassa():
     assert self.driver.find_element(By.CSS_SELECTOR, ".infraCaption").text == "Lista de Processos para Avaliação (34 registros):"
     for i in range(0, 30):
       self.driver.find_element(By.ID, "divInfraAreaDados").click
-      self.driver.find_element(By.XPATH, "//*[@id=\"divInfraAreaTabela\"]/table/tbody/tr[2]/td[9]/a[1]").click()
+      self.driver.find_element(By.XPATH, "//*[@id=\"divInfraAreaTabela\"]/table/tbody/tr[2]/td[10]/a[1]").click()
       #self.driver.find_element(By.ID, "sbmEliminacao").click()
       self.driver.switch_to.alert.accept()
       WebDriverWait(self.driver, 30).until(expected_conditions.visibility_of_element_located((By.XPATH, "//*[@id=\"divInfraBarraLocalizacao\"]")))
@@ -168,7 +168,7 @@ class Test1100SEIGDAvaliacaoMassa():
     assert self.driver.find_element(By.CSS_SELECTOR, ".infraCaption").text == "Lista de Processos para Avaliação (57 registros - 1 a 50):"
     for i in range(0, 25):
       self.driver.find_element(By.ID, "divInfraAreaDados").click()
-      self.driver.find_element(By.XPATH, "//*[@id=\"divInfraAreaTabela\"]/table/tbody/tr[2]/td[9]/a[1]").click()
+      self.driver.find_element(By.XPATH, "//*[@id=\"divInfraAreaTabela\"]/table/tbody/tr[2]/td[10]/a[1]").click()
       #self.driver.find_element(By.ID, "sbmEliminacao").click()
       self.driver.switch_to.alert.accept()
       WebDriverWait(self.driver, 30).until(expected_conditions.visibility_of_element_located((By.XPATH, "//*[@id=\"divInfraBarraLocalizacao\"]")))
