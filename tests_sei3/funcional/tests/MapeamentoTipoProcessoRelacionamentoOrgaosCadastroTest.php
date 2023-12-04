c00ef4b16291e2452b9a15050f59780da188e31f
caduvieira <3831408+caduvieira@users.noreply.github.com>
2023-12-04 17:52:24 -0300

    chore: adiciona badge do codesniffer
    feat: mapeamento de tipos de processo e relacionamento entre orgãos
    
    feat: mapeamento de tipos de processo e relacionamento entre orgãos

---
+++ tests_sei3/funcional/tests/MapeamentoTipoProcessoRelacionamentoOrgaosCadastroTest.php
@@ -0,0 +1,141 @@
+<?php
+
+/**
+ * Testes de mapeamento de tipos de processo e relacionamento entre orgãos
+ * Cadastro mapeamento de orgãos
+ */
+class MapeamentoTipoProcessoRelacionamentoOrgaosCadastroTest extends CenarioBaseTestCase
+{
+    public static $remetente;
+    public static $remetenteB;
+
+    /**
+     * @inheritdoc
+     * @return void
+     */
+    function setUp(): void
+    {
+        parent::setUp();
+        self::$remetente = $this->definirContextoTeste(CONTEXTO_ORGAO_A);
+        
+        $penMapUnidadesFixture = new PenMapUnidadesFixture(CONTEXTO_ORGAO_A);
+        $penMapUnidadesFixture->cadastrar([
+            'idOrgaoDestino' => self::$remetente['ID_UNIDADE_ORGAO_DESTINO'],
+            'nomeOrgaoDestino' => self::$remetente['NOME_UNIDADE_ORGAO_DESTINO']
+        ]);
+    }
+    
+    /**
+     * Teste de cadastro de novo mapeamento entre ogrãos
+     *
+     * @return void
+     */
+    public function test_cadastrar_novo_mapeamento_orgao_externo()
+    {
+        // Configuração do dados para teste do cenário
+        self::$remetente = $this->definirContextoTeste(CONTEXTO_ORGAO_A);
+        $this->acessarSistema(
+            self::$remetente['URL'],
+            self::$remetente['SIGLA_UNIDADE'],
+            self::$remetente['LOGIN'],
+            self::$remetente['SENHA']
+        );
+        $this->paginaCadastroOrgaoExterno->navegarCadastroOrgaoExterno();
+        $this->paginaCadastroOrgaoExterno->novoMapOrgao();
+        $this->paginaCadastroOrgaoExterno->setarParametros(
+            self::$remetente['REP_ESTRUTURAS'],
+            self::$remetente['NOME_UNIDADE_ESTRUTURA'],
+            self::$remetente['NOME_UNIDADE_ORGAO_DESTINO']
+        );
+        $this->paginaCadastroOrgaoExterno->salvar();
+
+        $orgaoOrigem = $this->paginaCadastroOrgaoExterno->buscarOrgaoOrigem(self::$remetente['NOME_UNIDADE_ESTRUTURA']);
+        $orgaoDestino = $this->paginaCadastroOrgaoExterno->buscarOrgaoDestino(self::$remetente['NOME_UNIDADE_ORGAO_DESTINO']);
+
+        $this->assertNotNull($orgaoOrigem);
+        $this->assertNotNull($orgaoDestino);
+        sleep(1);
+        $mensagemRetornoAlert = $this->paginaCadastroOrgaoExterno->buscarMensagemAlerta();
+        $this->assertStringContainsString(
+            utf8_encode('Relacionamento entre Órgãos cadastrado com sucesso.'),
+            $mensagemRetornoAlert
+        );
+    }
+
+    /**
+     * Teste para cadastro de mapeamento de orgão exteno já existente
+     *
+     * @group MapeamentoOrgaoExterno
+     *
+     * @return void
+     */
+    public function test_cadastrar_mapeamento_orgao_externo_ja_cadastrado()
+    {
+        self::$remetente = $this->definirContextoTeste(CONTEXTO_ORGAO_A);
+        $this->acessarSistema(
+            self::$remetente['URL'],
+            self::$remetente['SIGLA_UNIDADE'],
+            self::$remetente['LOGIN'],
+            self::$remetente['SENHA']
+        );
+        $this->paginaCadastroOrgaoExterno->navegarCadastroOrgaoExterno();
+        $this->paginaCadastroOrgaoExterno->novoMapOrgao();
+        $this->paginaCadastroOrgaoExterno->setarParametros(
+            self::$remetente['REP_ESTRUTURAS'],
+            self::$remetente['NOME_UNIDADE_ESTRUTURA'],
+            self::$remetente['NOME_UNIDADE_ORGAO_DESTINO']
+        );
+        $this->paginaCadastroOrgaoExterno->salvar();
+        sleep(1);
+        $mensagemRetornoAlert = $this->paginaCadastroOrgaoExterno->buscarMensagemAlerta();
+        $this->assertStringContainsString(
+            utf8_encode('Cadastro de relacionamento entre órgãos já existente.'),
+            $mensagemRetornoAlert
+        );
+    }
+
+    /**
+     * Teste para editar mapeamento de orgão exteno
+     *
+     * @group MapeamentoOrgaoExterno
+     *
+     * @return void
+     */
+    public function test_editar_mapeamento_orgao_externo()
+    {
+        self::$remetente = $this->definirContextoTeste(CONTEXTO_ORGAO_A);
+        self::$remetenteB = $this->definirContextoTeste(CONTEXTO_ORGAO_B);
+        $this->acessarSistema(
+            self::$remetente['URL'],
+            self::$remetente['SIGLA_UNIDADE'],
+            self::$remetente['LOGIN'],
+            self::$remetente['SENHA']
+        );
+        $this->paginaCadastroOrgaoExterno->navegarCadastroOrgaoExterno();
+
+        $this->paginaCadastroOrgaoExterno->editarMapOrgao();
+        $this->paginaCadastroOrgaoExterno->setarParametros(
+            self::$remetenteB['REP_ESTRUTURAS'],
+            self::$remetenteB['NOME_UNIDADE_ESTRUTURA'],
+            self::$remetente['NOME_UNIDADE_ORGAO_DESTINO']
+        );
+        $this->paginaCadastroOrgaoExterno->salvar();
+
+        $orgaoOrigem = $this->paginaCadastroOrgaoExterno->buscarOrgaoOrigem(self::$remetenteB['NOME_UNIDADE_ESTRUTURA']);
+        $orgaoDestino = $this->paginaCadastroOrgaoExterno->buscarOrgaoDestino(self::$remetente['NOME_UNIDADE_ORGAO_DESTINO']);
+
+        $this->assertNotNull($orgaoOrigem);
+        $this->assertNotNull($orgaoDestino);
+        sleep(1);
+        $mensagemRetornoAlert = $this->paginaCadastroOrgaoExterno->buscarMensagemAlerta();
+        $this->assertStringContainsString(
+            utf8_encode('Relacionamento entre Órgãos atualizado com sucesso.'),
+            $mensagemRetornoAlert
+        );
+    }
+
+    function tearDown(): void
+    {
+        parent::tearDown();
+    }
+}
---
+++ tests_super/funcional/src/fixtures/PenMapUnidadesFixture.php
@@ -0,0 +1,35 @@
+<?php
+
+class PenMapUnidadesFixture
+{
+    private static $contexto;
+
+    public function __construct(string $contexto)
+    {
+        self::$contexto = $contexto;
+    }
+
+    protected function inicializarObjInfraIBanco()
+    {
+        return \BancoSEI::getInstance();
+    }
+
+    public function cadastrar($dados = [])
+    {
+        $bancoOrgaoA = new DatabaseUtils(self::$contexto);
+        $bancoOrgaoA->execute(
+            "INSERT INTO md_pen_unidade (id_unidade, id_unidade_rh, nome_unidade_rh) ".
+            "VALUES(?, ?, ?)",
+            array(110000001, $dados['idOrgaoDestino'], $dados['nomeOrgaoDestino'])
+        );
+    }
+
+    public function deletar($dados = []): void
+    {
+        $bancoOrgaoA = new DatabaseUtils(self::$contexto);
+        $bancoOrgaoA->execute(
+            "DELETE FROM md_pen_unidade WHERE id_unidade = ? and id_unidade_rh = ?",
+            array(110000001, $dados['idOrgaoDestino'])
+        );
+    }
+}
